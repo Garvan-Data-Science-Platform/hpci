3d86f0b51f90c06521b873cb77c15c1f0e9a7655
ignatiusm <ignatius.menzies@gmail.com>
2024-06-26 10:54:14 +1200

    Try with cabal2nix

--- flake.nix
+++ flake.nix
@@ -1,83 +1,52 @@
 {
-  description = "hpci";
+  description = "HPCI";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    static-haskell-nix = {
-      url = "github:nh2/static-haskell-nix";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, static-haskell-nix }:
-    let
-      overlay = final: prev: {
-        haskell = prev.haskell // {
-          packageOverrides = hfinal: hprev:
-            prev.haskell.packageOverrides hfinal hprev // {
-              hpci = hfinal.callCabal2nix "hpci" ./. { };
-            };
-        };
-        hpci = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.hpci;
-      };
-      perSystem = system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
         let
-          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-          hspkgs = pkgs.haskellPackages;
-
-          staticPkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-          survey = import "${static-haskell-nix}/survey" {
-            compiler = "ghc948";
-            normalPkgs = staticPkgs;
-          };
-          appendConfigureFlags = drv: flags: drv.overrideAttrs (old: {
-            configureFlags = (old.configureFlags or []) ++ flags;
-          });
+            pkgs = nixpkgs.legacyPackages.${system}.pkgsMusl;
+            haskellPackages = pkgs.haskell.packages.ghc948;
+            packageName = "hpci";
+            jailbreakUnbreak = pkg: pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+            inherit (pkgs.haskell.lib) appendConfigureFlags justStaticExecutables;
+            mypackage = haskellPackages.callCabal2nix packageName self rec {
+            };
         in
         {
-          devShells = rec {
-            default = hpci-shell;
-            hpci-shell = hspkgs.shellFor {
-              withHoogle = true;
-              packages = p: [ p.hpci ];
-              buildInputs = [
-                hspkgs.cabal-install
-                hspkgs.haskell-language-server
-                hspkgs.hlint
-                hspkgs.ormolu
-                pkgs.bashInteractive
-                pkgs.pkg-config
-                pkgs.libssh2
-              ];
-            };
-          };
-          packages = rec {
-            default = hpci;
-            hpci = pkgs.hpci;
+          packages.${packageName} = pkgs.haskell.lib.overrideCabal mypackage (old: {
             enableSharedExecutables = false;
             enableSharedLibraries = false;
-            static = appendConfigureFlags survey.haskellPackages.hpci [
-              "--ghc-option=-optl=-static"
-              "--ghc-option=-optl=-pthread"
-              "--ghc-option=-fPIC"
-              "--ghc-option=-optc=-fPIC"
-              "--ghc-option=-optl=-static-libgcc"
-              "--ghc-option=-optl=-static-libstdc++"
-              "--ghc-option=-optl=-lm"
-              "--ghc-option=-optl=-lstdc++"
-              "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
-              "--extra-lib-dirs=${staticPkgs.zlib.static}/lib"
-              "--extra-lib-dirs=${staticPkgs.libssh2}/lib"
-              "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+            configureFlags =     [
+                  "--ghc-option=-optl=-static"
+                  "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
+                  "--extra-lib-dirs=${pkgs.libsshw.override { withStatic = true; }}/lib"
+                  "--extra-lib-dirs=${pkgs.zlib.static}/lib"
+                  "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+                  "--extra-lib-dirs=${pkgs.ncurses.override { enableStatic = true; }}/lib"
             ];
-            buildInputs = [
-              staticPkgs.pkg-config
-              staticPkgs.libssh2
-              staticPkgs.zlib
+          });
+
+          defaultPackage = self.packages.${system}.${packageName};
+          devShell = pkgs.mkShell {
+            buildInputs = with haskellPackages; [
+              haskell-language-server
+              cabal-install
+              cabal2nix
+              hlint
+              ormolu
+            ] ++ [ 
+              pkgs.zlib
+              pkgs.libssh2
+              pkgs.pkg-config
             ];
+            inputsFrom = builtins.attrValues self.packages.${system};
           };
-        };
-    in
-    { inherit overlay; } // flake-utils.lib.eachDefaultSystem perSystem;
+        }
+      );
 }
