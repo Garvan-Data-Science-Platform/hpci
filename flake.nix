e0390ec0cf7a3493138b02d22c50d0e51bc9f79d
ignatiusm <ignatius.menzies@gmail.com>
2024-06-26 11:20:42 +1200

    Try with cabal2nix updated

--- flake.nix
+++ flake.nix
@@ -10,24 +10,33 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-            overlay = final: prev: {
-              haskell = prev.haskell // {
-                packageOverrides = hfinal: hprev:
-                  prev.haskell.packageOverrides hfinal hprev // {
-                    hpci = hfinal.callCabal2nix "hpci" ./. { };
-                  };
+          overlay = final: prev: {
+            haskell = prev.haskell // {
+              packageOverrides = hfinal: hprev:
+                prev.haskell.packageOverrides hfinal hprev // {
+                  hpci = hfinal.callCabal2nix "hpci" ./. { };
+                };
+            };
+            hpci = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.hpci;
+          };
+          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+          staticPkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+            config = {
+              allowUnfree = true;
+              packageOverrides = pkgs: {
+                stdenv = pkgs.stdenvAdapters.makeStaticBinaries pkgs.stdenv;
               };
-              hpci = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.hpci;
             };
-            pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-            hspkgs = pkgs.haskellPackages;
-            staticPkgs = nixpkgs.legacyPackages.${system}.pkgsMusl;
-            haskellPackages = staticPkgs.haskell.packages.ghc948;
-            packageName = "hpci";
-            jailbreakUnbreak = pkg: staticPkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-            inherit (staticPkgs.haskell.lib) appendConfigureFlags justStaticExecutables;
-            mypackage = haskellPackages.callCabal2nix packageName self rec {
-            };
+          };
+          haskellPackages = staticPkgs.haskell.packages.ghc948;
+          packageName = "hpci";
+          jailbreakUnbreak = pkg: staticPkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+          inherit (staticPkgs.haskell.lib) appendConfigureFlags justStaticExecutables;
+          mypackage = haskellPackages.callCabal2nix packageName self rec {
+          };
         in
         {
           devShells = rec {
@@ -46,20 +55,20 @@
               ];
             };
           };
-          packages.${packageName} = staticPkgs.haskell.lib.overrideCabal mypackage (old: {
-            enableSharedExecutables = false;
-            enableSharedLibraries = false;
-            configureFlags =     [
-                  "--ghc-option=-optl=-static"
-                  "--extra-lib-dirs=${staticPkgs.gmp6.override { withStatic = true; }}/lib"
-                  "--extra-lib-dirs=${staticPkgs.libsshw.override { withStatic = true; }}/lib"
-                  "--extra-lib-dirs=${staticPkgs.zlib.static}/lib"
-                  "--extra-lib-dirs=${staticPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
-                  "--extra-lib-dirs=${staticPkgs.ncurses.override { enableStatic = true; }}/lib"
+          packages = rec {
+            default = hpci;
+            hpci = pkgs.hpci;
+            static = appendConfigureFlags (justStaticExecutables mypackage) [
+              "--ghc-option=-optl=-static"
+              "--extra-lib-dirs=${staticPkgs.gmp6.override { withStatic = true; }}/lib"
+              "--extra-lib-dirs=${staticPkgs.libssh2.override { withStatic = true; }}/lib"
+              "--extra-lib-dirs=${staticPkgs.zlib.static}/lib"
+              "--extra-lib-dirs=${staticPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+              "--extra-lib-dirs=${staticPkgs.ncurses.override { enableStatic = true; }}/lib"
             ];
-          });
+          };
 
-          defaultPackage = self.packages.${system}.${packageName};
+          defaultPackage = self.packages.${system}.hpci;
           devShell = pkgs.mkShell {
             buildInputs = with haskellPackages; [
               haskell-language-server
@@ -67,7 +76,7 @@
               cabal2nix
               hlint
               ormolu
-            ] ++ [ 
+            ] ++ [
               pkgs.zlib
               pkgs.libssh2
               pkgs.pkg-config
