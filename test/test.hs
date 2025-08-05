ee91059b7ec25fb3e18346fbb72eb13cdf384579
ignatiusm <ignatius.menzies@gmail.com>
2025-08-05 16:19:03 +1200

    Add integration test

---
+++ test/test.hs
@@ -0,0 +1,11 @@
+module Main (
+  main
+) where
+
+import Test.Tasty
+import Test.Tasty.Program
+
+main :: IO ()
+main = defaultMain $ testGroup "Test with dockerised OpenPBS" $ [
+    testProgram "hpci" "make" ["test-schedule"] Nothing
+  ]
