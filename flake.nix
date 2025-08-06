ed2d9750505d0c25019f3f287744ae2d599b6e2e
ignatiusm <ignatius.menzies@gmail.com>
2025-08-06 12:22:56 +1200

    Remove redundant test option from flake

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
               enableSharedExecutables = false;
               enableSharedLibraries = false;
               doCheck = true;
-              testTarget = "--test-options=-e";
               configureFlags = [
                 "--ghc-option=-optl=-static"
                 "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
