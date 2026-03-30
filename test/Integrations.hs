fe1e69832326661c10461ed14f0475f6855e75b5
ignatiusm <ignatius.menzies@gmail.com>
2026-03-30 17:56:18 +1300

    Adds slurm submission fail test

--- test/Integrations.hs
+++ test/Integrations.hs
@@ -6,7 +6,8 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Text (pack, unpack, isInfixOf)
 import qualified Data.Text.Lazy as LazyText
 import System.Directory (makeAbsolute)
-import System.Process (readProcess)
+import System.Exit (ExitCode(..))
+import System.Process (readProcess, readProcessWithExitCode)
 import Test.Hspec
 import TestContainers.Hspec
 import TestContainers.Docker as D
@@ -43,14 +44,36 @@ setupContainers = do
   pbs   <- run (pbsContainerReq absolutePathToKey)
   pure (slurm, pbs)
 
+runHpci :: [String] -> IO (ExitCode, String, String)
+runHpci args = readProcessWithExitCode "cabal" (["run", "hpci-exe", "--"] ++ args) ""
+
 integrationSpec :: Spec
 integrationSpec = describe "Docker Integration Tests" $ do
   around (withContainers setupContainers) $ do
 
-    it "has the correct version of Slurm installed" $ \(slurm, _pbs) -> do
-      output <- runInContainer slurm ["sinfo", "--version"]
-      pack output `shouldSatisfy` ("slurm" `isInfixOf`)
+    -- it "has the correct version of Slurm installed" $ \(slurm, _pbs) -> do
+    --   output <- runInContainer slurm ["sinfo", "--version"]
+    --   pack output `shouldSatisfy` ("slurm" `isInfixOf`)
 
-    it "has the correct version of PBS installed" $ \(_slurm, pbs) -> do
-      output <- runInContainer pbs ["pbsnodes", "--version"]
-      pack output `shouldSatisfy` ("version" `isInfixOf`)
+    -- it "has the correct version of PBS installed" $ \(_slurm, pbs) -> do
+    --   output <- runInContainer pbs ["pbsnodes", "--version"]
+    --   pack output `shouldSatisfy` ("version" `isInfixOf`)
+
+    it "crashes when there is a job submission error" $ \(slurm, _pbs) -> do
+      let port = show $ D.containerPort slurm 22
+
+      let args = [ "--user", "root"
+			           , "--host", "127.0.0.1"
+			           , "--port", port
+			           , "--publicKey", "test_key.pub"
+			           , "--privateKey", "test_key"
+			           , "schedule"
+			           , "--scheduler", "slurm"
+			           , "--script", "ci/test_fail.slurm"
+			           , "--logFile", "test_job.log"
+			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+                ]
+
+      (exitCode, stdout, stderr) <- runHpci args
+
+      exitCode `shouldBe` ExitFailure 1
