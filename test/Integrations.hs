49f586e2ce54d77ed47c11db283d83f9c40fe43a
ignatiusm <ignatius.menzies@gmail.com>
2026-04-05 17:43:51 +1200

    tidy up tests

--- test/Integrations.hs
+++ test/Integrations.hs
@@ -3,11 +3,11 @@
 module Integrations (integrationSpec) where
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Text (pack, unpack)
+import Data.Text (pack)
 import qualified Data.Text.Lazy as LazyText
 import System.Directory (makeAbsolute)
 import System.Exit (ExitCode(..))
-import System.Process (readProcess, readProcessWithExitCode)
+import System.Process (readProcessWithExitCode)
 import Test.Hspec
 import TestContainers.Hspec
 import TestContainers.Docker as D
@@ -46,50 +46,50 @@ integrationSpec :: Spec
 integrationSpec = describe "Docker Integration Tests" $ do
   aroundAll (withContainers setupContainers) $ do
 
-    -- it "succeeds for a normal slurm job" $ \(slurm, _pbs) -> do
-    --   let port = show $ D.containerPort slurm 22
+    it "succeeds for a normal slurm job" $ \(slurm, _pbs) -> do
+      let sshPort = show $ D.containerPort slurm 22
 
-    --   let args = [ "--user", "root"
-			 --           , "--host", "127.0.0.1"
-			 --           , "--port", port
-			 --           , "--publicKey", "test_key.pub"
-			 --           , "--privateKey", "test_key"
-			 --           , "schedule"
-			 --           , "--scheduler", "slurm"
-			 --           , "--script", "ci/test_job.slurm"
-			 --           , "--logFile", "test_job.log"
-			 --           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-    --             ]
+      let cliArgs = [ "--user", "root"
+			           , "--host", "127.0.0.1"
+			           , "--port", sshPort
+			           , "--publicKey", "test_key.pub"
+			           , "--privateKey", "test_key"
+			           , "schedule"
+			           , "--scheduler", "slurm"
+			           , "--script", "ci/test_job.slurm"
+			           , "--logFile", "test_job.log"
+			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+                ]
 
-    --   (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
 
-    --   jobExitCode `shouldBe` ExitSuccess
+      jobExitCode `shouldBe` ExitSuccess
 
-    -- it "succeeds for a normal pbs job" $ \(_slurm, pbs) -> do
-    --   let port = show $ D.containerPort pbs 22
+    it "succeeds for a normal pbs job" $ \(_slurm, pbs) -> do
+      let sshPort = show $ D.containerPort pbs 22
 
-    --   let args = [ "--user", "pbsuser"
-			 --           , "--host", "127.0.0.1"
-			 --           , "--port", port
-			 --           , "--publicKey", "test_key.pub"
-			 --           , "--privateKey", "test_key"
-			 --           , "schedule"
-			 --           , "--scheduler", "pbs"
-			 --           , "--script", "ci/test_job.pbs"
-			 --           , "--logFile", "test_job.log"
-			 --           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-    --             ]
+      let cliArgs = [ "--user", "pbsuser"
+			           , "--host", "127.0.0.1"
+			           , "--port", sshPort
+			           , "--publicKey", "test_key.pub"
+			           , "--privateKey", "test_key"
+			           , "schedule"
+			           , "--scheduler", "pbs"
+			           , "--script", "ci/test_job.pbs"
+			           , "--logFile", "test_job.log"
+			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+                ]
 
-    --   (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
 
-    --   jobExitCode `shouldBe` ExitSuccess
+      jobExitCode `shouldBe` ExitSuccess
 
     it "crashes when there is a slurm submission error" $ \(slurm, _pbs) -> do
-      let port = show $ D.containerPort slurm 22
+      let sshPort = show $ D.containerPort slurm 22
 
-      let args = [ "--user", "root"
+      let cliArgs = [ "--user", "root"
 			           , "--host", "127.0.0.1"
-			           , "--port", port
+			           , "--port", sshPort
 			           , "--publicKey", "test_key.pub"
 			           , "--privateKey", "test_key"
 			           , "schedule"
@@ -99,16 +99,16 @@ integrationSpec = describe "Docker Integration Tests" $ do
 			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
                 ]
 
-      (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
 
       jobExitCode `shouldBe` ExitFailure 1
 
     it "crashes when there is a pbs job submission error" $ \(_slurm, pbs) -> do
-      let port = show $ D.containerPort pbs 22
+      let sshPort = show $ D.containerPort pbs 22
 
-      let args = [ "--user", "pbsuser"
+      let cliArgs = [ "--user", "pbsuser"
 			           , "--host", "127.0.0.1"
-			           , "--port", port
+			           , "--port", sshPort
 			           , "--publicKey", "test_key.pub"
 			           , "--privateKey", "test_key"
 			           , "schedule"
@@ -118,16 +118,16 @@ integrationSpec = describe "Docker Integration Tests" $ do
 			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
                 ]
 
-      (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
 
       jobExitCode `shouldBe` ExitFailure 1
 
-    it "succeeds when a '--directive' successfully overrides a slurm submission error" $ \(slurm, _pbs) -> do
-      let port = show $ D.containerPort slurm 22
+    it "succeeds when a '--scheduler-arg' successfully overrides a slurm submission error" $ \(slurm, _pbs) -> do
+      let sshPort = show $ D.containerPort slurm 22
 
-      let args = [ "--user", "root"
+      let cliArgs = [ "--user", "root"
 			           , "--host", "127.0.0.1"
-			           , "--port", port
+			           , "--port", sshPort
 			           , "--publicKey", "test_key.pub"
 			           , "--privateKey", "test_key"
 			           , "schedule"
@@ -139,16 +139,16 @@ integrationSpec = describe "Docker Integration Tests" $ do
 			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
                 ]
 
-      (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
 
       jobExitCode `shouldBe` ExitSuccess
 
-    it "succeeds when a '--directive' overrides a pbs job submission error" $ \(_slurm, pbs) -> do
-      let port = show $ D.containerPort pbs 22
+    it "succeeds when a '--scheduler-arg' overrides a pbs job submission error" $ \(_slurm, pbs) -> do
+      let sshPort = show $ D.containerPort pbs 22
 
-      let args = [ "--user", "pbsuser"
+      let cliArgs = [ "--user", "pbsuser"
 			           , "--host", "127.0.0.1"
-			           , "--port", port
+			           , "--port", sshPort
 			           , "--publicKey", "test_key.pub"
 			           , "--privateKey", "test_key"
 			           , "schedule"
@@ -160,6 +160,46 @@ integrationSpec = describe "Docker Integration Tests" $ do
 			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
                 ]
 
-      (jobExitCode, _, _) <- runHpci args
+      (jobExitCode, _, _) <- runHpci cliArgs
+
+      jobExitCode `shouldBe` ExitSuccess
+
+    -- it "escapes tricky characters in slurm '--scheduler-arg'" $ \(slurm, _pbs) -> do
+    --   let sshPort = show $ D.containerPort slurm 22
+
+    --   let cliArgs = [ "--user", "root"
+			 --           , "--host", "127.0.0.1"
+			 --           , "--port", sshPort
+			 --           , "--publicKey", "test_key.pub"
+			 --           , "--privateKey", "test_key"
+			 --           , "schedule"
+			 --           , "--scheduler", "slurm"
+			 --           , "--script", "ci/test_job.slurm"
+			 --           , "--logFile", "test_job.log"
+    --              , "--scheduler-arg", "--job-name='Build&Test'"
+			 --           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+    --             ]
+
+    --   (jobExitCode, _, _) <- runHpci cliArgs
+
+    --   jobExitCode `shouldBe` ExitSuccess
+
+    it "escapes tricky characters in pbs '--scheduler-arg'" $ \(_slurm, pbs) -> do
+      let sshPort = show $ D.containerPort pbs 22
+
+      let cliArgs = [ "--user", "pbsuser"
+			           , "--host", "127.0.0.1"
+			           , "--port", sshPort
+			           , "--publicKey", "test_key.pub"
+			           , "--privateKey", "test_key"
+			           , "schedule"
+			           , "--scheduler", "pbs"
+			           , "--script", "ci/test_job.pbs"
+			           , "--logFile", "test_job.log"
+                 , "--scheduler-arg", "-l 'walltime=01:30:00'"
+			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+                ]
+
+      (jobExitCode, _, _) <- runHpci cliArgs
 
       jobExitCode `shouldBe` ExitSuccess
