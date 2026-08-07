9d1ba5a4d9900c90c4e5bb86570cdeebfdb40ce9
ignatiusm <ignatius.menzies@gmail.com>
2026-08-07 16:16:07 +1200

    Refactor integration tests

--- test/Integrations.hs
+++ test/Integrations.hs
@@ -24,184 +24,64 @@ setupContainerTargets = pure
 runHpci :: [String] -> IO (ExitCode, String, String)
 runHpci inputArgs = readProcessWithExitCode "cabal" (["run", "hpci-exe", "--"] ++ inputArgs) ""
 
+baseScheduleArgs :: SshTarget -> String -> String -> [String] -> [String]
+baseScheduleArgs target scheduler script extraArgs =
+  [ "--user", targetUser target
+	, "--host", "127.0.0.1"
+	, "--port", targetPort target
+	, "--publicKey", "test_key.pub"
+	, "--privateKey", "test_key"
+	, "schedule"
+	, "--scheduler", scheduler
+	, "--script", script
+	, "--logFile", "test_job.log"
+	, "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
+  ] ++ extraArgs
+
+shouldExitWith :: [String] -> ExitCode -> IO()
+shouldExitWith args expectedCode = do
+  (actualCode, _, _) <- runHpci args
+  actualCode `shouldBe` expectedCode
+
 integrationSpec :: Spec
 integrationSpec = describe "Docker Integration Tests" $ do
   before setupContainerTargets $ do
 
-    it "succeeds for a normal slurm job" $ \(slurm, _pbs) -> do
+    context "Normal Job Execution" $ do
+      it "succeeds for a normal slurm job" $ \(slurm, _pbs) -> do
+        baseScheduleArgs slurm "slurm" "ci/test_job.slurm" [] `shouldExitWith` ExitSuccess
 
-      let cliArgs = [ "--user", targetUser slurm
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort slurm
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "slurm"
-			           , "--script", "ci/test_job.slurm"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
+      it "succeeds for a normal pbs job" $ \(_slurm, pbs) -> do
+        baseScheduleArgs pbs "pbs" "ci/test_job.pbs" [] `shouldExitWith` ExitSuccess
 
-      (jobExitCode, _, _) <- runHpci cliArgs
+    context "Submission Error (Wrong Partition/Queue)" $ do
+      it "crashes when there is a slurm submission error (wrong partition)" $ \(slurm, _pbs) -> do
+        baseScheduleArgs slurm "slurm" "ci/test_fail.slurm" [] `shouldExitWith` (ExitFailure 1)
 
-      jobExitCode `shouldBe` ExitSuccess
+      it "crashes when there is a pbs job submission error (wrong queue)" $ \(_slurm, pbs) -> do
+        baseScheduleArgs pbs "pbs" "ci/test_fail.pbs" [] `shouldExitWith` (ExitFailure 1)
 
-    it "succeeds for a normal pbs job" $ \(_slurm, pbs) -> do
+    context "Submission Error (Non-zero Exit)" $ do
+      it "crashes when there is a slurm submission error (non-zero exit)" $ \(slurm, _pbs) -> do
+        baseScheduleArgs slurm "slurm" "ci/test_exit.slurm" [] `shouldExitWith` (ExitFailure 7)
 
-      let cliArgs = [ "--user", targetUser pbs
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort pbs
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "pbs"
-			           , "--script", "ci/test_job.pbs"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
+      it "crashes when there is a pbs job submission error (non-zero exit)" $ \(_slurm, pbs) -> do
+        baseScheduleArgs pbs "pbs" "ci/test_exit.pbs" [] `shouldExitWith` (ExitFailure 7)
 
-      (jobExitCode, _, _) <- runHpci cliArgs
+    context "Scheduler Argument Overrides" $ do
+      it "succeeds when a '--scheduler-arg' successfully overrides a slurm submission error" $ \(slurm, _pbs) -> do
+        let extra = ["--scheduler-arg", "--partition=all", "--scheduler-arg", "--job-name=testjob"]
+        baseScheduleArgs slurm "slurm" "ci/test_fail.slurm" extra `shouldExitWith` ExitSuccess
 
-      jobExitCode `shouldBe` ExitSuccess
+      it "succeeds when a '--scheduler-arg' overrides a pbs job submission error" $ \(_slurm, pbs) -> do
+        let extra = ["--scheduler-arg", "-q workq", "--scheduler-arg", "-N testjob"]
+        baseScheduleArgs pbs "pbs" "ci/test_fail.pbs" extra `shouldExitWith` ExitSuccess
 
-    it "crashes when there is a slurm submission error (wrong partition)" $ \(slurm, _pbs) -> do
-      let cliArgs = [ "--user", targetUser slurm
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort slurm
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "slurm"
-			           , "--script", "ci/test_fail.slurm"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
+    context "Scheduler Argument Escaping" $ do
+      it "escapes tricky characters in slurm '--scheduler-arg'" $ \(slurm, _pbs) -> do
+        let extra = ["--scheduler-arg", "--job-name='Build&Test'"]
+        baseScheduleArgs slurm "slurm" "ci/test_job.slurm" extra `shouldExitWith` ExitSuccess
 
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitFailure 1
-
-    it "crashes when there is a slurm submission error (non-zero exit)" $ \(slurm, _pbs) -> do
-      let cliArgs = [ "--user", targetUser slurm
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort slurm
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "slurm"
-			           , "--script", "ci/test_exit.slurm"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitFailure 7
-
-    it "crashes when there is a pbs job submission error (wrong queue)" $ \(_slurm, pbs) -> do
-      let cliArgs = [ "--user", targetUser pbs
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort pbs
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "pbs"
-			           , "--script", "ci/test_fail.pbs"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitFailure 1
-
-    it "crashes when there is a pbs job submission error (non-zero exit)" $ \(_slurm, pbs) -> do
-      let cliArgs = [ "--user", targetUser pbs
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort pbs
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "pbs"
-			           , "--script", "ci/test_exit.pbs"
-			           , "--logFile", "test_job.log"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitFailure 7
-
-    it "succeeds when a '--scheduler-arg' successfully overrides a slurm submission error" $ \(slurm, _pbs) -> do
-      let cliArgs = [ "--user", targetUser slurm
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort slurm
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "slurm"
-			           , "--script", "ci/test_fail.slurm"
-			           , "--logFile", "test_job.log"
-                 , "--scheduler-arg", "--partition=all"
-                 , "--scheduler-arg", "--job-name=testjob"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitSuccess
-
-    it "succeeds when a '--scheduler-arg' overrides a pbs job submission error" $ \(_slurm, pbs) -> do
-      let cliArgs = [ "--user", targetUser pbs
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort pbs
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "pbs"
-			           , "--script", "ci/test_fail.pbs"
-			           , "--logFile", "test_job.log"
-                 , "--scheduler-arg", "-q workq"
-                 , "--scheduler-arg", "-N testjob"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitSuccess
-
-    it "escapes tricky characters in slurm '--scheduler-arg'" $ \(slurm, _pbs) -> do
-      let cliArgs = [ "--user", targetUser slurm
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort slurm
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "slurm"
-			           , "--script", "ci/test_job.slurm"
-			           , "--logFile", "test_job.log"
-                 , "--scheduler-arg", "--job-name='Build&Test'"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitSuccess
-
-    it "escapes tricky characters in pbs '--scheduler-arg'" $ \(_slurm, pbs) -> do
-      let cliArgs = [ "--user", targetUser pbs
-			           , "--host", "127.0.0.1"
-			           , "--port", targetPort pbs
-			           , "--publicKey", "test_key.pub"
-			           , "--privateKey", "test_key"
-			           , "schedule"
-			           , "--scheduler", "pbs"
-			           , "--script", "ci/test_job.pbs"
-			           , "--logFile", "test_job.log"
-                 , "--scheduler-arg", "-l 'walltime=01:30:00'"
-			           , "-c", "TEST_VAR1=success,TEST_VAR2=double_success"
-                ]
-
-      (jobExitCode, _, _) <- runHpci cliArgs
-
-      jobExitCode `shouldBe` ExitSuccess
+      it "escapes tricky characters in pbs '--scheduler-arg'" $ \(_slurm, pbs) -> do
+        let extra = ["--scheduler-arg", "-l 'walltime=01:30:00'"]
+        baseScheduleArgs pbs "pbs" "ci/test_job.pbs" extra `shouldExitWith` ExitSuccess
