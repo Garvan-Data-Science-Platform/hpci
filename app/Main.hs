1427a34a2fade836f310f4c84147f5b941408869
ignatiusm <ignatius.menzies@gmail.com>
2026-08-11 11:28:17 +1200

    WIP adds improved error messages and tests for incorrect port and ssh keys

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,26 @@
+module Main where
+
+import Control.Exception (catch, displayException)
+import System.Exit (exitFailure)
+
 import Cli
 import Exec
 import Schedule
+import Errors (wrapSshErrors, HpciUserError)
 
 runHpci :: Options -> IO ()
-runHpci opts = do
+runHpci opts = wrapSshErrors $ do
   case optCommand opts of 
     Exec execStr -> runExec opts execStr
 
     _            -> runSchedule opts
 
+handleHpciError :: HpciUserError -> IO ()
+handleHpciError e = do
+  putStrLn $ displayException e
+  exitFailure
+
 main :: IO ()
 main = do
   opts <- parseOptions
-  runHpci opts
+  runHpci opts `catch` handleHpciError
